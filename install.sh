5de7aeedb1fea79852b73a5a1297e77c6b1eff55
turbo <turbochoo@qq.com>
2023-05-05 16:36:51 +0800

    doom emacs init

---
+++ install.sh
@@ -0,0 +1,30 @@
+sudo pacman --needed --noconfirm -S sqlite ripgrep fd wmctrl exa languagetool zstd ctags git-delta tidy
+# Rust
+rustup update
+rustup component add rls rust-analysis rust-src rust-analyzer
+
+# Javascript
+npm i -g typescript typescript-language-server
+
+# Vue
+npm install vue-language-server -g
+
+# Bash
+npm i -g bash-language-server
+
+# C++ use clangd
+brew install llvm
+
+npm install -g cspell prettier
+
+pip3 install --upgrade pylint gnureadline black cpplint
+
+# python
+npm i -g pyright
+pip3 install --user debugpy
+
+
+# gopls for (+lsp)
+go install golang.org/x/tools/gopls@latest
+
+#go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
